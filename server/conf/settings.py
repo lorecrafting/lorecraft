785ce9a1ac801660dbba75d12e99c020d18a988d
Raymond Luong <raymond.n.luong@gmail.com>
2023-03-19 21:31:53 -1000

    Add django_svelte to installed_apps

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -34,6 +34,7 @@ from evennia.settings_default import *
 # This is the name of your game. Make it catchy!
 SERVERNAME = "lorecraft"
 
+INSTALLED_APPS += ('django_svelte',)
 
 ######################################################################
 # Settings given in secret_settings.py override those in this file.
