5b7cbff8250f49424141ead7380887836e89c5f7
Raymond Luong <raymond.n.luong@gmail.com>
2023-03-26 16:20:08 -1000

    Add jquery and Evennia.js to new webclient

--- web/webclient/new_webclient.py
+++ web/webclient/new_webclient.py
@@ -1,5 +1,10 @@
-# in mygame/web/story.py
+from django.conf import settings
+from django.http import Http404
 from django.shortcuts import render
 
 def webclient(request):
-    return render(request, "new_webclient.html")
+    if not settings.WEBCLIENT_ENABLED:
+        raise Http404
+
+    pagevars = {"browser_sessid": request.session.session_key}
+    return render(request, "new_webclient.html", pagevars)
