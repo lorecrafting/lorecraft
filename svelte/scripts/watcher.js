64191391691fc2e776f99149c5f627ea3d624e63
Raymond Luong <raymond.n.luong@gmail.com>
2023-03-26 17:14:29 -1000

    Add file watcher for svelte src files, build and evennia collectstatic on save

---
+++ svelte/scripts/watcher.js
@@ -0,0 +1,17 @@
+const watch = require('node-watch');
+const { exec } = require("child_process");
+
+watch('./src', { recursive: true }, function(evt, name) {
+    console.log('%s changed.', name);
+    exec("npm run build;evennia collectstatic --noinput", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+  });
