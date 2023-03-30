ae96552647f5ce346be325604466fff4c9bbdc12
Raymond Luong <raymond.n.luong@gmail.com>
2023-03-29 22:33:12 -1000

    Dynamically update room UI when contents arrive or leave

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -21,4 +21,42 @@ class Room(ObjectParent, DefaultRoom):
     properties and methods available on all Objects.
     """
 
+
+
+
+    # TODO: Refactor at_object_receive and at_objecte leave
+    def at_object_receive(self, moved_obj, source_location, move_type="move", **kwargs):
+        characters = [character for character in self.contents if character.typename == "Character"]
+
+        if len(characters) > 0:
+            room = self
+            room_data = {
+                "dbref": room.id,
+                "name": room.name,
+                "desc": room.db.desc,
+                "contents":  list(set(room.contents) - set(room.exits) - set(room.contents_get(content_type="character"))),
+                "characters": list(set(room.contents_get(content_type="character"))),
+                "exits": room.exits,
+            }
+
+            for char in characters:
+                char.msg(room_data=room_data)
+    pass
+
+    def at_object_leave(self, moved_obj, source_location, move_type="move", **kwargs):
+        characters = [character for character in self.contents if character.typename == "Character"]
+        print(moved_obj)
+        if len(characters) > 0:
+            room = self
+            room_data = {
+                "dbref": room.id,
+                "name": room.name,
+                "desc": room.db.desc,
+                "contents":  list(set(room.contents) - set([moved_obj]) - set(room.exits) - set(room.contents_get(content_type="character"))),
+                "characters": list(set(room.contents_get(content_type="character")) - set([moved_obj])),
+                "exits": room.exits,
+            }
+
+            for char in characters:
+                char.msg(room_data=room_data)
     pass
